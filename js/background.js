cf2fc7159dbf5c06a9146eb67a5810dc2d017a3b
Daniel Hickman <danielhickman9@gmail.com>
2015-07-03 15:13:46 -0700

    First replacing content script

--- js/background.js
+++ js/background.js
@@ -1,9 +1,21 @@
+// chrome.runtime.onMessage.addListener(
+// 	function(request, sender, sendResponse) {
+// 		if (request.toggle == "true") {
+// 			toggle = true;
+// 		} else if (request.toggle == "false") {
+// 			toggle = false;
+// 		}
+// });
+
+// if (toggle === undefined || toggle === null) {
+// 	toggle = false;
+// };
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if (changeInfo.status == "complete") {
-		alert(tab.url);
-		alert(tabId);
-		// chrome.tabs.executeScript({
-		// 	code: ''
-		// });
+		// if (toggle) {
+			chrome.tabs.sendMessage(tabId, {url: tab.url, id: tabId})
+			chrome.tabs.executeScript(null, {file: "/js/content_script.js"});
+		// };
 	}
 }); 
--- js/content_script.js
+++ js/content_script.js
@@ -1,5 +1,17 @@
-function loop (tab, url) {
-	if (url < 0) {
-		url = document.URL;
-	};
-}
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if (! request.url) {
+			url = document.URL;
+		} else {
+			url = request.url;
+		}
+		if (! request.id) {
+			// nothing
+		} else {
+			id = request.id;
+		}
+		// alert(url + id);
+		if (/^https:\/\/github.com/i.test(url)) {
+			document.getElementsByClassName('header-logo-invertocat')[0].innerHTML = '<img src="chrome-extension://affgcnnkppghdeajoheclcgjbcceonij/img/logos/pridetocat.png" width="28" height="28" class="octicon-mark-github pridetocat" alt="GitHub">';
+		};
+});
