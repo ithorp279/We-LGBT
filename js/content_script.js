35cfaf42d43fa58fe86e584aaf9f1776b4ad1d5c
Daniel Hickman <danielhickman9@gmail.com>
2015-07-23 20:41:35 -0700

    Subdomains in Logo, Improved SO, Added Logos

--- js/content_script.js
+++ js/content_script.js
@@ -10,15 +10,39 @@ chrome.runtime.onMessage.addListener(
 		} else {
 			id = request.id;
 		}
-		// alert(url + id);
-
+		// ---- WEB LOGOS ----
 		// GitHub
-		if (/^(http)(s)?(:\/\/)github.com/i.test(url)) {
+		if (/^(http)(s)?(:\/\/)(\w+\.)*github.com/i.test(url)) {
 			document.getElementsByClassName('header-logo-invertocat')[0].innerHTML = '<img src="' + chrome.extension.getURL("/img/logos/github.png") + '" width="28" height="28" class="octicon-mark-github pridetocat" alt="GitHub">';
 		};
-
 		// StackOverflow
-		if (/^(http)(s)?(:\/\/)stackoverflow.com/i.test(url)) {
+		if (/^(http)(s)?(:\/\/)(\w+\.)*stackoverflow.com/i.test(url)) {
 			document.getElementById('hlogo').childNodes[1].style.backgroundImage = 'url(' + chrome.extension.getURL("/img/logos/stackoverflow.svg") + ')';
 		};
+		// Apple
+		if (/^(http)(s)?(:\/\/)(\w+\.)*apple.com/i.test(url)) {
+			document.getElementsByClassName('gh-tab-link')[0].style.backgroundImage = 'url(' + chrome.extension.getURL("/img/logos/apple.svg") + ')';
+		};
+
+		// ---- TWITTER LOGOS ----
+		// Mojang
+		if (/^(http)(s)?(:\/\/)(\w+\.)*twitter.com\/Mojang/i.test(url)) {
+			document.getElementsByClassName('ProfileAvatar-image')[0].src = chrome.extension.getURL("/img/logos/mojang.png");
+		};
+		// Reddit
+		if (/^(http)(s)?(:\/\/)(\w+\.)*twitter.com\/reddit/i.test(url)) {
+			document.getElementsByClassName('ProfileAvatar-image')[0].src = chrome.extension.getURL("/img/logos/reddit.png");
+		};
+
+		// ---- GOOGLE+ LOGOS ----
+		// Reddit
+		if (/^(http)(s)?(:\/\/)plus.google.com\/\+reddit/i.test(url)) {
+			document.getElementsByClassName('fa-kz Zxa')[0].src = chrome.extension.getURL("/img/logos/reddit.png");
+		};
+		// BuzzFeed
+		if (/^(http)(s)?(:\/\/)plus.google.com\/\+BuzzFeed/i.test(url)) {
+			document.getElementsByClassName('fa-kz Zxa')[0].src = chrome.extension.getURL("/img/logos/buzzfeed.png");
+		};
+
+		// ---- FACEBOOK LOGOS ----
 });
