691eebecf837883cd607795a63920f9baa5283fa
Daniel Hickman <danielhickman9@gmail.com>
2015-12-16 15:23:52 -0800

    Indenting

--- src/js/background.js
+++ src/js/background.js
@@ -1,233 +1,233 @@
 /* global chrome */
 (function() {
-  "use strict";
+	"use strict";
 
-  var su;
-  var it;
-  var ns;
-  var rep;
-  var url;
-  var traffic;
-  var trafficSu;
-  var trafficIt;
-  var trafficNs;
-  var trafficNf;
-  var trafficPercents;
+	var su;
+	var it;
+	var ns;
+	var rep;
+	var url;
+	var traffic;
+	var trafficSu;
+	var trafficIt;
+	var trafficNs;
+	var trafficNf;
+	var trafficPercents;
 
-  function setBrowserActionIcon(iconName) {
-    chrome.browserAction.setIcon({
-      "path": {
-        "19": "/img/icons/" + iconName + "/icon_19.png",
-        "38": "/img/icons/" + iconName + "/icon_38.png"
-      }
-    });
-  }
+	function setBrowserActionIcon(iconName) {
+		chrome.browserAction.setIcon({
+			"path": {
+				"19": "/img/icons/" + iconName + "/icon_19.png",
+				"38": "/img/icons/" + iconName + "/icon_38.png"
+			}
+		});
+	}
 
-  function setBrowserActionTitle(newTitle) {
-    chrome.browserAction.setTitle({
-      "title": newTitle
-    });
-  }
+	function setBrowserActionTitle(newTitle) {
+		chrome.browserAction.setTitle({
+			"title": newTitle
+		});
+	}
 
-  function getJSON(fileUrl, local) {
-    return new Promise(function(resolve, reject) {
-      function handleError(connectionError) {
-        var usingBackup = typeof local === "undefined";
-        var message = (connectionError ? "There was a connection error of some sort" : "We reached our target server, but it returned an error") +
-          (usingBackup ? ". Using" : " and there wasn't a") +
-          " backup local file.";
-        if (usingBackup) {
-          console.warn(message);
-          resolve(getJSON(local, null));
-        } else {
-          console.error(message);
-          reject();
-        }
-      }
+	function getJSON(fileUrl, local) {
+		return new Promise(function(resolve, reject) {
+			function handleError(connectionError) {
+				var usingBackup = typeof local === "undefined";
+				var message = (connectionError ? "There was a connection error of some sort" : "We reached our target server, but it returned an error") +
+					(usingBackup ? ". Using" : " and there wasn't a") +
+					" backup local file.";
+				if (usingBackup) {
+					console.warn(message);
+					resolve(getJSON(local, null));
+				} else {
+					console.error(message);
+					reject();
+				}
+			}
 
-      var request = new XMLHttpRequest();
-      request.open("GET", fileUrl, true);
+			var request = new XMLHttpRequest();
+			request.open("GET", fileUrl, true);
 
-      request.addEventListener("load", function() {
-        if (request.status >= 200 && request.status < 400) {
-          // Success!
-          var data = JSON.parse(request.response);
-          resolve(data);
-          return;
-        }
-        handleError(false);
-      });
+			request.addEventListener("load", function() {
+				if (request.status >= 200 && request.status < 400) {
+					// Success!
+					var data = JSON.parse(request.response);
+					resolve(data);
+					return;
+				}
+				handleError(false);
+			});
 
-      request.addEventListener("error", function() {
-        handleError(true);
-      });
+			request.addEventListener("error", function() {
+				handleError(true);
+			});
 
-      request.send();
-    });
-  }
+			request.send();
+		});
+	}
 
-  function getPatterns(data) {
-    su.data = data.supportive;
-    it.data = data.intermediate;
-    ns.data = data["not supportive"];
+	function getPatterns(data) {
+		su.data = data.supportive;
+		it.data = data.intermediate;
+		ns.data = data["not supportive"];
 
-    var obj = [su, it, ns];
-    for (var o in [su, it, ns]) {
-      for (var i in obj[o].data) {
-        var dataI = typeof obj[o].data[i];
-        if (typeof dataI === "string") {
-          obj[o].array.push(dataI.replace(/\./g, "\\."));
-        } else {
-          if (typeof dataI === "object") {
-            if (typeof dataI.url === "string") {
-              obj[o].array.push(dataI.url.replace(/\./g, "\\."));
-            } else {
-              if (Array.isArray(dataI.url)) {
-                for (var u in dataI.url) {
-                  obj[o].array.push(dataI.url[u].replace(/\./g, "\\."));
-                }
-              }
-            }
-          }
-        }
-      }
-      if (obj[o].array.length > 0) {
-        obj[o].regex = new RegExp("^(http)?(s)?(:\\/\\/)?(\\w+\\.)*(" + obj[o].array.join("|") + ")", "i");
-      }
-    }
-  }
+		var obj = [su, it, ns];
+		for (var o in [su, it, ns]) {
+			for (var i in obj[o].data) {
+				var dataI = typeof obj[o].data[i];
+				if (typeof dataI === "string") {
+					obj[o].array.push(dataI.replace(/\./g, "\\."));
+				} else {
+					if (typeof dataI === "object") {
+						if (typeof dataI.url === "string") {
+							obj[o].array.push(dataI.url.replace(/\./g, "\\."));
+						} else {
+							if (Array.isArray(dataI.url)) {
+								for (var u in dataI.url) {
+									obj[o].array.push(dataI.url[u].replace(/\./g, "\\."));
+								}
+							}
+						}
+					}
+				}
+			}
+			if (obj[o].array.length > 0) {
+				obj[o].regex = new RegExp("^(http)?(s)?(:\\/\\/)?(\\w+\\.)*(" + obj[o].array.join("|") + ")", "i");
+			}
+		}
+	}
 
-  // Patterns
-  if (typeof su === "undefined" || typeof it === "undefined" || typeof ns === "undefined") {
-    su = {
-      "data": [],
-      "array": [],
-      "regex": ""
-    };
-    it = {
-      "data": [],
-      "array": [],
-      "regex": ""
-    };
-    ns = {
-      "data": [],
-      "array": [],
-      "regex": ""
-    };
+	// Patterns
+	if (typeof su === "undefined" || typeof it === "undefined" || typeof ns === "undefined") {
+		su = {
+			"data": [],
+			"array": [],
+			"regex": ""
+		};
+		it = {
+			"data": [],
+			"array": [],
+			"regex": ""
+		};
+		ns = {
+			"data": [],
+			"array": [],
+			"regex": ""
+		};
 
-    // Get Patterns
-    getJSON("https://raw.githubusercontent.com/xorprojects/We-LGBT/master/src/js/rep.json", "/js/rep.json").then(getPatterns);
-  }
+		// Get Patterns
+		getJSON("https://raw.githubusercontent.com/xorprojects/We-LGBT/master/src/js/rep.json", "/js/rep.json").then(getPatterns);
+	}
 
-  function getObjFromData(data) {
-    for (var i in data) {
-      if (typeof data[i] === "object") {
-        var urlsData = data[i].url;
-        var urlsDataString = Array.isArray(urlsData) ? urlsData.join("|") : urlsData;
-        var regex = new RegExp("^(http)?(s)?(:\\/\\/)?(\\w+\\.)*(" + urlsDataString + ")", "i");
-        if (regex.test(url)) {
-          return data[i];
-        }
-      }
-    }
-  }
+	function getObjFromData(data) {
+		for (var i in data) {
+			if (typeof data[i] === "object") {
+				var urlsData = data[i].url;
+				var urlsDataString = Array.isArray(urlsData) ? urlsData.join("|") : urlsData;
+				var regex = new RegExp("^(http)?(s)?(:\\/\\/)?(\\w+\\.)*(" + urlsDataString + ")", "i");
+				if (regex.test(url)) {
+					return data[i];
+				}
+			}
+		}
+	}
 
-  function getObj() {
-    var dataParents = {
-      "su": su,
-      "it": it,
-      "ns": ns
-    };
-    return getObjFromData(dataParents[rep].data);
-  }
+	function getObj() {
+		var dataParents = {
+			"su": su,
+			"it": it,
+			"ns": ns
+		};
+		return getObjFromData(dataParents[rep].data);
+	}
 
-  // Variables
-  if (typeof traffic === "undefined" || typeof trafficSu === "undefined" || typeof trafficIt === "undefined" || typeof trafficNs === "undefined" || typeof trafficNf === "undefined") {
-    traffic = 0;
-    trafficSu = 0;
-    trafficIt = 0;
-    trafficNs = 0;
-    trafficNf = 0;
-  }
-  if (typeof rep === "undefined" || typeof url === "undefined") {
-    rep = "nf";
-    url = "example.com";
-  }
+	// Variables
+	if (typeof traffic === "undefined" || typeof trafficSu === "undefined" || typeof trafficIt === "undefined" || typeof trafficNs === "undefined" || typeof trafficNf === "undefined") {
+		traffic = 0;
+		trafficSu = 0;
+		trafficIt = 0;
+		trafficNs = 0;
+		trafficNf = 0;
+	}
+	if (typeof rep === "undefined" || typeof url === "undefined") {
+		rep = "nf";
+		url = "example.com";
+	}
 
-  // Recive requests for cards from popup
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.message === "open") {
-      trafficPercents = [trafficSu / traffic * 100, trafficIt / traffic * 100, trafficNs / traffic * 100, trafficNf / traffic * 100];
-      sendResponse({
-        "url": url,
-        "rep": rep,
-        "traffic": traffic,
-        "trafficSu": trafficPercents[0],
-        "trafficIt": trafficPercents[1],
-        "trafficNs": trafficPercents[2],
-        "trafficNf": trafficPercents[3],
-        "obj": getObj()
-      });
-    }
-  });
+	// Recive requests for cards from popup
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		if (request.message === "open") {
+			trafficPercents = [trafficSu / traffic * 100, trafficIt / traffic * 100, trafficNs / traffic * 100, trafficNf / traffic * 100];
+			sendResponse({
+				"url": url,
+				"rep": rep,
+				"traffic": traffic,
+				"trafficSu": trafficPercents[0],
+				"trafficIt": trafficPercents[1],
+				"trafficNs": trafficPercents[2],
+				"trafficNf": trafficPercents[3],
+				"obj": getObj()
+			});
+		}
+	});
 
-  // Main
-  function main() {
-    // Test Against Regular Expression
-    if (!su.regex == "" && su.regex.test(url)) {
-      rep = "su";
-    } else if (!it.regex == "" && it.regex.test(url)) {
-      rep = "it";
-    } else if (!ns.regex == "" && ns.regex.test(url)) {
-      rep = "ns";
-    } else {
-      rep = "nf";
-    }
-    traffic++;
+	// Main
+	function main() {
+		// Test Against Regular Expression
+		if (!su.regex == "" && su.regex.test(url)) {
+			rep = "su";
+		} else if (!it.regex == "" && it.regex.test(url)) {
+			rep = "it";
+		} else if (!ns.regex == "" && ns.regex.test(url)) {
+			rep = "ns";
+		} else {
+			rep = "nf";
+		}
+		traffic++;
 
-    // Browser Action Icon and Title
-    // Supportive
-    if (rep === "su") {
-      setBrowserActionIcon("supportive");
-      setBrowserActionTitle("Supportive - We LGBT");
-      trafficSu++;
-    }
-    // Intermediate
-    if (rep === "it") {
-      setBrowserActionIcon("intermediate");
-      setBrowserActionTitle("Intermediate - We LGBT");
-      trafficIt++;
-    }
-    // Not Supportive
-    if (rep === "ns") {
-      setBrowserActionIcon("not-supportive");
-      setBrowserActionTitle("Not Supportive - We LGBT");
-      trafficNs++;
-    }
-    // Not Found
-    if (rep === "nf") {
-      setBrowserActionIcon("not-found");
-      setBrowserActionTitle("Not Found - We LGBT");
-      trafficNf++;
-    }
-  }
+		// Browser Action Icon and Title
+		// Supportive
+		if (rep === "su") {
+			setBrowserActionIcon("supportive");
+			setBrowserActionTitle("Supportive - We LGBT");
+			trafficSu++;
+		}
+		// Intermediate
+		if (rep === "it") {
+			setBrowserActionIcon("intermediate");
+			setBrowserActionTitle("Intermediate - We LGBT");
+			trafficIt++;
+		}
+		// Not Supportive
+		if (rep === "ns") {
+			setBrowserActionIcon("not-supportive");
+			setBrowserActionTitle("Not Supportive - We LGBT");
+			trafficNs++;
+		}
+		// Not Found
+		if (rep === "nf") {
+			setBrowserActionIcon("not-found");
+			setBrowserActionTitle("Not Found - We LGBT");
+			trafficNf++;
+		}
+	}
 
-  // When URL Changed
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status === "complete") {
-      url = tab.url;
-      main();
-    }
-  });
+	// When URL Changed
+	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+		if (changeInfo.status === "complete") {
+			url = tab.url;
+			main();
+		}
+	});
 
-  // When Tab Changed
-  chrome.tabs.onActivated.addListener(function() {
-    chrome.tabs.query({
-      "active": true,
-      "lastFocusedWindow": true
-    }, function(tabs) {
-      url = tabs[0].url;
-      main();
-    });
-  });
+	// When Tab Changed
+	chrome.tabs.onActivated.addListener(function() {
+		chrome.tabs.query({
+			"active": true,
+			"lastFocusedWindow": true
+		}, function(tabs) {
+			url = tabs[0].url;
+			main();
+		});
+	});
 })();
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,63 +1,63 @@
 /* global chrome */
 (function() {
-  "use strict";
+	"use strict";
 
-  var repAboutPData = {
-    "su": "This site is supportive.",
-    "it": "This site is intermediate.",
-    "ns": "This site is not supportive.",
-    "nf": "This site isn't in our filter."
-  };
+	var repAboutPData = {
+		"su": "This site is supportive.",
+		"it": "This site is intermediate.",
+		"ns": "This site is not supportive.",
+		"nf": "This site isn't in our filter."
+	};
 
-  function init() {
-    chrome.runtime.sendMessage({
-      "message": "open"
-    }, function(response) {
-      var url = response.url;
-      var rep = response.rep;
-      var obj = response.obj;
-      var traffic = response.traffic;
-      var trafficSu = response.trafficSu;
-      var trafficIt = response.trafficIt;
-      var trafficNs = response.trafficNs;
-      var trafficNf = response.trafficNf;
-      var domain = url.replace(/^(http)?(s)?(:\/\/)?(www\.)?/i, "").replace(/(\/.*)$/i, "");
+	function init() {
+		chrome.runtime.sendMessage({
+			"message": "open"
+		}, function(response) {
+			var url = response.url;
+			var rep = response.rep;
+			var obj = response.obj;
+			var traffic = response.traffic;
+			var trafficSu = response.trafficSu;
+			var trafficIt = response.trafficIt;
+			var trafficNs = response.trafficNs;
+			var trafficNf = response.trafficNf;
+			var domain = url.replace(/^(http)?(s)?(:\/\/)?(www\.)?/i, "").replace(/(\/.*)$/i, "");
 
-      var at = document.getElementById("about-title");
-      var ap = document.getElementById("about-p");
-      var aa = document.getElementById("about-a");
-      var cc = document.getElementById("contribute-card");
-      var cp = document.getElementById("contribute-p");
-      var ca = document.getElementById("contribute-a");
+			var at = document.getElementById("about-title");
+			var ap = document.getElementById("about-p");
+			var aa = document.getElementById("about-a");
+			var cc = document.getElementById("contribute-card");
+			var cp = document.getElementById("contribute-p");
+			var ca = document.getElementById("contribute-a");
 
-      if (typeof obj === "undefined") {
-        at.textContent = "About This Site";
-        aa.parentElement.classList.add("hidden");
-        cc.classList.remove("hidden");
-        cp.textContent = "We don't have any information for " + domain + ", could you contribute what you know about it?";
-        ca.href = "https://github.com/xorprojects/We-LGBT/issues/new?title=Missing%20Information%20For:" + encodeURIComponent(domain) + "&body=-%20Name:%20%0A-%20Description:%20";
-        ap.textContent = repAboutPData[rep];
-      } else {
-        at.innerHTML = obj.name;
-        cc.classList.add("hidden");
-        ap.innerHTML = obj.description;
-        if (typeof obj["custom action"] === "undefined") {
-          aa.parentElement.classList.add("hidden");
-        } else {
-          aa.parentElement.classList.remove("hidden");
-          aa.href = obj["custom action"].href;
-          aa.innerHTML = obj["custom action"].innerHTML;
-        }
-      }
+			if (typeof obj === "undefined") {
+				at.textContent = "About This Site";
+				aa.parentElement.classList.add("hidden");
+				cc.classList.remove("hidden");
+				cp.textContent = "We don't have any information for " + domain + ", could you contribute what you know about it?";
+				ca.href = "https://github.com/xorprojects/We-LGBT/issues/new?title=Missing%20Information%20For:" + encodeURIComponent(domain) + "&body=-%20Name:%20%0A-%20Description:%20";
+				ap.textContent = repAboutPData[rep];
+			} else {
+				at.innerHTML = obj.name;
+				cc.classList.add("hidden");
+				ap.innerHTML = obj.description;
+				if (typeof obj["custom action"] === "undefined") {
+					aa.parentElement.classList.add("hidden");
+				} else {
+					aa.parentElement.classList.remove("hidden");
+					aa.href = obj["custom action"].href;
+					aa.innerHTML = obj["custom action"].innerHTML;
+				}
+			}
 
-      if (typeof traffic === "number" && traffic > 0) {
-        document.getElementById("webtraffic").textContent = "Your web traffic this session is " + Math.round(trafficSu) + "% supportive, " + Math.round(trafficIt) + "% intermediate, " + Math.round(trafficNs) + "% not supportive and, " + Math.round(trafficNf) + "% with no found standing. We don't record your web traffic, this is only calculated by our URL filter. (Rounded)";
-      }
-    });
-    document.getElementById("closepopup").addEventListener("click", function() {
-      window.close();
-    });
-  }
+			if (typeof traffic === "number" && traffic > 0) {
+				document.getElementById("webtraffic").textContent = "Your web traffic this session is " + Math.round(trafficSu) + "% supportive, " + Math.round(trafficIt) + "% intermediate, " + Math.round(trafficNs) + "% not supportive and, " + Math.round(trafficNf) + "% with no found standing. We don't record your web traffic, this is only calculated by our URL filter. (Rounded)";
+			}
+		});
+		document.getElementById("closepopup").addEventListener("click", function() {
+			window.close();
+		});
+	}
 
-  document.addEventListener("DOMContentLoaded", init);
+	document.addEventListener("DOMContentLoaded", init);
 })();
